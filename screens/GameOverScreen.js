b5ba6739875039592466d85f343366c2030f46cd
jerome <jerome.henry3@gmail.com>
2019-11-20 00:13:24 +0100

    -

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, StyleSheet, Button, Image } from 'react-native';
+import { View, StyleSheet, Text, Button, Image } from 'react-native';
 import BodyText from '../components/BodyText';
-
+import colors from '../constants/colors';
 
 const GameOverScreen = props => {
 
@@ -11,12 +11,18 @@ const GameOverScreen = props => {
             <View style={styles.imageContainer}>
                 <Image
                     style={styles.image}
+                    fadeDuration={800}
                     resizeMode='cover'
-                    source={require('../assets/img/success.png')}
+                    // source={require('../assets/img/success.png')}
+                    source={{uri: 'https://www.ternelia.com/sites/default/files/styles/crop_1400x688/public/landing_page/banniere-groupe-montagne.jpg'}}
                 />
             </View>
-            <BodyText>Nombre d'essais: {props.roundsNumber}</BodyText>
-            <BodyText>Le nombre était : {props.userNumber}</BodyText>
+            <BodyText>Nombre d'essais: <Text style={styles.highlight}>
+                {props.roundsNumber}
+            </Text></BodyText>
+            <BodyText>Le nombre était : <Text style={styles.highlight}>
+                {props.userNumber}
+            </Text></BodyText>
             <Button title='Nouvelle partie' onPress={props.onRestart} />
         </View>
     )
@@ -40,6 +46,11 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%',
+    },
+    highlight: {
+        color: colors.primary,
+        fontFamily: 'open-sans-bold',
+        marginBottom: 5,
     }
 })
 
